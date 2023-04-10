e54f97cd1012703985c01bd8239aacf56908e2a1
Nubdubi <kojun1412@naver.com>
2023-04-10 11:10:57 +0900

    cd

---
+++ src/data/index.js
@@ -0,0 +1,11 @@
+const data = {
+  products: [
+    { id: "1", name: "hey", category: "baby" },
+    { id: "2", name: "hey2", category: "baby2" },
+    { id: "3", name: "hey2", category: "baby2" },
+    { id: "4", name: "hey2", category: "baby2" },
+    { id: "5", name: "hey2", category: "baby2" },
+  ],
+};
+
+export default data;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,27 @@
+import data from "../data.js";
+// 이게 에러남
+const HomeScreen = {
+  render: () => {
+    const { products, error } = data;
+    if (error) {
+      return `<div> error </div>`;
+    }
+    return `
+      <ul class="products">
+        ${products
+          .map(
+            (product) =>
+              ` <div class="product">
+              <div class=""productname">
+                  ${product.name}
+                  </div>
+                  ${product.category}
+                </div>`
+          )
+          .join("\n")}
+      
+    `;
+  },
+};
+
+export default HomeScreen;
